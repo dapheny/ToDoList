f57ea3b49a37a8b5fe1ccc2143d23f3b6f2a8eda
Dapheny Wono <daphenywono@Daphenys-MacBook-Pro.local>
2015-05-21 16:45:18 -0700

    upload
    tutorial for objective C

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Dapheny Wono  on 4/29/15.
+//  Copyright (c) 2015 Dapheny Wono . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Dapheny Wono  on 4/29/15.
+//  Copyright (c) 2015 Dapheny Wono . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
